40a96217b089d273f213447943ef7f8b6db783d4
student <student@lab-45-7.cs.put.poznan.pl>
2019-11-04 17:13:22 +0100

    changed comments

--- Program.java
+++ Program.java
@@ -5,8 +5,8 @@ public class Program {
     //and a comment to the comment
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        // Prints "Hello, World" to the terminal window. but it does so in a weird way
+        System.out.println("whoooooooo");
     }
-
+    //added a comment
 }
